6a1f1eca18bbf5e4b5c9254f40853bbb1c37987f
darmariduan <dharmarhiduwhan@gmail.com>
2022-06-03 12:26:20 +0700

    StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,8 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: Center(
             // child: Text('Halo Dunia'),
-            child: Heading(text: 'Hallo Dunia')),
+            // child: Heading(text: 'Hallo Dunia')),
+            child: BiggerText(text: 'Halo Dunia')),
       ),
     );
   }
@@ -32,6 +33,37 @@ class Heading extends StatelessWidget {
         fontSize: 24.0,
         fontWeight: FontWeight.bold,
       ),
+    );
+  }
+}
+
+class BiggerText extends StatefulWidget {
+  final String text;
+
+  const BiggerText({required this.text});
+
+  @override
+  _BiggerTextState createState() => _BiggerTextState();
+}
+
+class _BiggerTextState extends State<BiggerText> {
+  double _textSize = 16.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(widget.text, style: TextStyle(fontSize: _textSize)),
+        ElevatedButton(
+          child: Text('Perbesar'),
+          onPressed: () {
+            setState(() {
+              _textSize = 32.0;
+            });
+          },
+        )
+      ],
     );
   }
 }
