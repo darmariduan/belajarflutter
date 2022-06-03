aa72d6febfd1a129a91488791787e4b46d4dadaf
darmariduan <dharmarhiduwhan@gmail.com>
2022-06-03 14:55:57 +0700

    Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -6,64 +6,45 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Demo Flutter',
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        body: Center(
-            // child: Text('Halo Dunia'),
-            // child: Heading(text: 'Hallo Dunia')),
-            child: BiggerText(text: 'Halo Dunia')),
-      ),
+      home: FirstScreen(), //Call this FirstScreen
     );
   }
 }
 
-class Heading extends StatelessWidget {
-  final String text;
-
-  Heading({required this.text});
-
+class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: TextStyle(
-        fontSize: 24.0,
-        fontWeight: FontWeight.bold,
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            color: Colors.white,
+          ),
+          onPressed: () {},
+        ),
+        title: Text('First Screen'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+            onPressed: () {},
+          )
+        ],
       ),
-    );
-  }
-}
-
-class BiggerText extends StatefulWidget {
-  final String text;
-
-  const BiggerText({required this.text});
-
-  @override
-  _BiggerTextState createState() => _BiggerTextState();
-}
-
-class _BiggerTextState extends State<BiggerText> {
-  double _textSize = 16.0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(widget.text, style: TextStyle(fontSize: _textSize)),
-        ElevatedButton(
-          child: Text('Perbesar'),
-          onPressed: () {
-            setState(() {
-              _textSize = 32.0;
-            });
-          },
-        )
-      ],
+      body: Center(
+        child: Text('Halo Dunia'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
     );
   }
 }
