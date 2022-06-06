90bca558582527cfa1e3e9de80297662667ee886
darmariduan <dharmarhiduwhan@gmail.com>
2022-06-06 20:59:17 +0700

    Column

--- lib/main.dart
+++ lib/main.dart
@@ -6,53 +6,74 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: FirstScreen(), //Call this FirstScreen
+      title: 'Wisata Bandung',
+      theme: ThemeData(),
+      home: DetailScreen(),
     );
   }
 }
 
-class FirstScreen extends StatelessWidget {
+class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(
-            Icons.menu,
-            color: Colors.white,
-          ),
-          onPressed: () {},
-        ),
-        title: Text('First Screen'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.search,
-              color: Colors.white,
+        body: SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+              margin: EdgeInsets.only(top: 16.0),
+              child: Text(
+                'Farm House Lembang',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              )),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Icon(Icons.calendar_today),
+                    SizedBox(
+                      height: 8.0,
+                    ),
+                    Text('Open Everyday'),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Icon(Icons.access_time),
+                    SizedBox(
+                      height: 8.0,
+                    ),
+                    Text('09:00 - 20:00')
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Icon(Icons.monetization_on),
+                    SizedBox(height: 8.0),
+                    Text('Rp 25.000'),
+                  ],
+                ),
+              ],
             ),
-            onPressed: () {},
-          )
+          ),
+          Container(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16.0),
+            ),
+          ),
         ],
       ),
-      body: Container(
-        child: Text(
-          'Hi',
-          style: TextStyle(fontSize: 40),
-        ),
-        decoration: BoxDecoration(
-          color: Colors.red,
-          border: Border.all(color: Colors.green, width: 3),
-          borderRadius: BorderRadius.circular(10),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {},
-      ),
-    );
+    ));
   }
 }
