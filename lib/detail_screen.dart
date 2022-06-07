8678c22ac7d4811e287aa96d199d66b6e131ca30
darmariduan <dharmarhiduwhan@gmail.com>
2022-06-08 03:01:04 +0700

    hero animation

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -11,119 +11,122 @@ class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Stack(
-                children: <Widget>[
-                  Image.asset(place.imageAsset),
-                  SafeArea(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          CircleAvatar(
-                            backgroundColor: Colors.grey,
-                            child: IconButton(
-                              icon: Icon(
-                                Icons.arrow_back,
-                                color: Colors.white,
-                              ),
-                              onPressed: () {
-                                Navigator.pop(context);
-                              },
-                            ),
-                          ),
-                          FavoriteButton(),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 16.0),
-                child: Text(
-                  place.name,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 30.0,
-                    fontFamily: 'Staatliches',
-                  ),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 16.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: Hero(
+        tag: 'pp',
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Stack(
                   children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        Icon(Icons.calendar_today),
-                        SizedBox(
-                          height: 8.0,
+                    Image.asset(place.imageAsset),
+                    SafeArea(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: Colors.grey,
+                              child: IconButton(
+                                icon: Icon(
+                                  Icons.arrow_back,
+                                  color: Colors.white,
+                                ),
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                              ),
+                            ),
+                            FavoriteButton(),
+                          ],
                         ),
-                        Text(
-                          place.openDays,
-                          style: informationTextStyle,
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: <Widget>[
-                        Icon(Icons.access_time),
-                        SizedBox(
-                          height: 8.0,
-                        ),
-                        Text(
-                          place.openTime,
-                          style: informationTextStyle,
-                        )
-                      ],
-                    ),
-                    Column(
-                      children: <Widget>[
-                        Icon(Icons.monetization_on),
-                        SizedBox(height: 8.0),
-                        Text(
-                          place.ticketPrice,
-                          style: informationTextStyle,
-                        ),
-                      ],
+                      ),
                     ),
                   ],
                 ),
-              ),
-              Container(
-                padding: EdgeInsets.all(16.0),
-                child: Text(
-                  place.description,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontFamily: 'Oxygen',
+                Container(
+                  margin: EdgeInsets.only(top: 16.0),
+                  child: Text(
+                    place.name,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      fontFamily: 'Staatliches',
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                height: 150,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: place.imageUrls.map((url) {
-                    return Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: Image.network(url),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          Icon(Icons.calendar_today),
+                          SizedBox(
+                            height: 8.0,
+                          ),
+                          Text(
+                            place.openDays,
+                            style: informationTextStyle,
+                          ),
+                        ],
                       ),
-                    );
-                  }).toList(),
+                      Column(
+                        children: <Widget>[
+                          Icon(Icons.access_time),
+                          SizedBox(
+                            height: 8.0,
+                          ),
+                          Text(
+                            place.openTime,
+                            style: informationTextStyle,
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Icon(Icons.monetization_on),
+                          SizedBox(height: 8.0),
+                          Text(
+                            place.ticketPrice,
+                            style: informationTextStyle,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+                Container(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text(
+                    place.description,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontFamily: 'Oxygen',
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 150,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: place.imageUrls.map((url) {
+                      return Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.network(url),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -18,35 +18,38 @@ class MainScreen extends StatelessWidget {
                 return DetailScreen(place: place);
               }));
             },
-            child: Card(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Expanded(
-                    flex: 1,
-                    child: Image.asset(place.imageAsset),
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Text(
-                            place.name,
-                            style: TextStyle(fontSize: 16.0),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text(place.location),
-                        ],
+            child: Hero(
+              tag: 'pp',
+              child: Card(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Expanded(
+                      flex: 1,
+                      child: Image.asset(place.imageAsset),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Text(
+                              place.name,
+                              style: TextStyle(fontSize: 16.0),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(place.location),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           );
