c3b71fd11ad104c13d49a28994a50b12156c3afc
darmariduan <dharmarhiduwhan@gmail.com>
2022-06-10 01:21:21 +0700

    tampilan web clean

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -10,6 +10,177 @@ class DetailScreen extends StatelessWidget {
   DetailScreen({required this.place});
   @override
   Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        if (constraints.maxWidth > 800) {
+          return DetailWebPage(place: place);
+        } else {
+          return DetailMobilePage(place: place);
+        }
+      },
+    );
+  }
+}
+
+class DetailWebPage extends StatefulWidget {
+  final TourismPlace place;
+
+  const DetailWebPage({required this.place});
+
+  @override
+  State<DetailWebPage> createState() => _DetailWebPageState();
+}
+
+class _DetailWebPageState extends State<DetailWebPage> {
+  final _scrollController = ScrollController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 16,
+          horizontal: 64,
+        ),
+        child: Center(
+          child: Container(
+            width: 1200,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Stack(
+                  children: <Widget>[
+                    Image.asset(widget.place.imageAsset),
+                    SafeArea(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: Colors.grey,
+                              child: IconButton(
+                                icon: Icon(
+                                  Icons.arrow_back,
+                                  color: Colors.white,
+                                ),
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                              ),
+                            ),
+                            FavoriteButton(),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 16.0),
+                  child: Text(
+                    widget.place.name,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      fontFamily: 'Staatliches',
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          Icon(Icons.calendar_today),
+                          SizedBox(
+                            height: 8.0,
+                          ),
+                          Text(
+                            widget.place.openDays,
+                            style: informationTextStyle,
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Icon(Icons.access_time),
+                          SizedBox(
+                            height: 8.0,
+                          ),
+                          Text(
+                            widget.place.openTime,
+                            style: informationTextStyle,
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Icon(Icons.monetization_on),
+                          SizedBox(height: 8.0),
+                          Text(
+                            widget.place.ticketPrice,
+                            style: informationTextStyle,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text(
+                    widget.place.description,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontFamily: 'Oxygen',
+                    ),
+                  ),
+                ),
+                Scrollbar(
+                  isAlwaysShown: true,
+                  controller: _scrollController,
+                  child: Container(
+                    height: 150,
+                    padding: const EdgeInsets.only(bottom: 16),
+                    child: ListView(
+                      controller: _scrollController,
+                      scrollDirection: Axis.horizontal,
+                      children: widget.place.imageUrls.map((url) {
+                        return Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Image.network(url),
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+}
+
+class DetailMobilePage extends StatelessWidget {
+  final TourismPlace place;
+  const DetailMobilePage({required this.place});
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Hero(
         tag: 'pp',
